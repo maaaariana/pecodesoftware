aaedfa9aaa1ecf869ace2eca51a7bfee4be67c94
maaaariana <pm75@i.ua>
2021-01-06 01:07:02 +0200

    Add empty branch

--- cypress/models/general.js
+++
@@ -1,8 +0,0 @@
-
-export function getSomeElements(){
-    cy.get('#about_us_id').should('exist');
-    cy.get('#about_us_id').should('contain.text', 'Turning your ideas into reality');
-
-    cy.get('#services_id').should('exist');
-    cy.get('#services_id').should('contain.text', 'Our services')
-}
