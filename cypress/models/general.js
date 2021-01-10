38b2c55555e32c7acc227f32343ed70755b98de7
maaaariana <pm75@i.ua>
2021-01-10 18:48:57 +0200

    Add sendMessage function

--- cypress/models/general.js
+++ cypress/models/general.js
@@ -1,8 +1,39 @@
 
 export function getSomeElements(){
-    cy.get('#about_us_id').should('exist');
-    cy.get('#about_us_id').should('contain.text', 'Turning your ideas into reality');
+    cy.get("body").then($body => {
+        if ($body.find("#about_us_id").length > 0) { 
+            cy.get('#about_us_id').should('exist');
+            cy.get('#about_us_id').should('contain.text', 'Turning your ideas into reality');
+        } else {
+            sendMessage();
+        }
+    });
 
-    cy.get('#services_id').should('exist');
-    cy.get('#services_id').should('contain.text', 'Our services')
+}
+
+export function sendMessage(){
+    var channelId = "G01HMB98XFV";
+    //some trouble with token
+    var token = "xoxp-347900769271-414824017409-918623091043-48c2a53f592d24a06398ee3bcfe51c85"; 
+    var url = "https://slack.com/api/chat.postMessage";
+
+    var headers = {
+        "Authorization": "Bearer " + token,
+        "Content-Type" : "application/json; charset=utf-8",
+    }
+
+    var body = {
+        "channel": channelId,
+        "text": "Something's wrong with pecode software website"
+    }
+
+    cy.request({
+        "method": 'POST',
+        "url": url, 
+        "headers": headers,
+        "body": JSON.stringify(body)
+    })
+    .then((response) => {
+        expect(response).property('status').to.equal(200);
+    });
 }
